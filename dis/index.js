3de5115387ef0444bec1201428d4fa7bfcc386c2
Lucas Santos <lucas201273@gmail.com>
2025-05-10 17:39:25 -0300

    chore: add jest

--- dis/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Person {
-    speak(name) {
-        return `Olá ${name?.toUpperCase() ?? "Fulano .."}!!!`;
-    }
-}
-const person = new Person();
-person.speak();
-person.speak('Lucas');
---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    moduleNameMapper:{
+      '@/tests/(.+)': '<rootDir>/tests/$1',
+      '@/(.+)': '<rootDir>/src/$1'
+    },
+    roots:[
+      '<rootDir>/src',
+      '<rootDir>/tests'],
+    testEnvironment: 'node',
+    clearMocks: true,
+    transform: {
+      '\\.ts$': 'ts-jest'
+    }
+};
+
+module.exports = config;
