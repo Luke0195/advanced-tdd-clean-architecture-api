a4ac763ee4d1ac7765457103223596d45ce418ed
Lucas Santos <lucas201273@gmail.com>
2025-05-11 11:15:15 -0300

    test: ensure FacebookAtuhentication calls LoadUserByEmailRepository when loadFacebookUserApi returns data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,16 +1,27 @@
+
 import { FacebookAuthenticationService } from '@/data/services'
 import { LoadFacebookUserApi } from '@/data/contracts/api'
+import { LoadUserAccountRepository } from '@/data/contracts/repositories'
 import { AuthenticatioError } from "@/domain/errors";
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FacebookAuthenticationService', () => {
   let sut:FacebookAuthenticationService;
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
+  let loadUserAccountRepository: MockProxy<LoadUserAccountRepository>;
+
   const token = 'any_token'
 
   beforeEach(() => {
     loadFacebookUserApi = mock();
-    sut = new FacebookAuthenticationService(loadFacebookUserApi);
+    loadUserAccountRepository = mock();
+    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepository);
+    loadFacebookUserApi.loadUser.mockResolvedValue(( {
+      name: 'any_facebook_name',
+      email: 'any_facebook_email@mail.com',
+      facebookId: 'any_facebook_id'
+      }
+    ))
   })
 
   test('Should calls loadUserFacebookApi with correct params', async () => {
@@ -26,9 +37,15 @@ describe('FacebookAuthenticationService', () => {
   });
 
   test('Should returns AuthenticationError when loadUserFacebookApi returns undefined', async() => {
-      const authResult = await sut.perform({ token});
+      const authResult = await sut.perform({ token });
       expect(authResult).toEqual(new AuthenticatioError());
   })
 
+  test('Should calls LoadUserByEmailRepository when loadFacebookUserApi returns data', async() => {
+      await sut.perform({token});
+      expect(loadUserAccountRepository.load).toHaveBeenCalledWith({ email: 'any_facebook_email@mail.com'});
+      expect(loadUserAccountRepository.load).toHaveBeenCalledTimes(1);
+    });
 
 });
+
