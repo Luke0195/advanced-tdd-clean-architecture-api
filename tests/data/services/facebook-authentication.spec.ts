7cf03cd386c5cb19ee615c39939171f1138172b4
Lucas Santos <lucas201273@gmail.com>
2025-05-11 11:32:15 -0300

    test: ensure FacebookAuthenticationService when LoadUserAccountRepository is undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,7 +1,7 @@
 
 import { FacebookAuthenticationService } from '@/data/services'
 import { LoadFacebookUserApi } from '@/data/contracts/api'
-import { LoadUserAccountRepository } from '@/data/contracts/repositories'
+import { LoadUserAccountRepository, CreateFacebookAccountRepository} from '@/data/contracts/repositories'
 import { AuthenticatioError } from "@/domain/errors";
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -9,13 +9,15 @@ describe('FacebookAuthenticationService', () => {
   let sut:FacebookAuthenticationService;
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
   let loadUserAccountRepository: MockProxy<LoadUserAccountRepository>;
+  let createFacebookAccountRepository:MockProxy<CreateFacebookAccountRepository>
 
   const token = 'any_token'
 
   beforeEach(() => {
     loadFacebookUserApi = mock();
     loadUserAccountRepository = mock();
-    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepository);
+    createFacebookAccountRepository = mock();
+    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepository, createFacebookAccountRepository);
     loadFacebookUserApi.loadUser.mockResolvedValue(( {
       name: 'any_facebook_name',
       email: 'any_facebook_email@mail.com',
@@ -47,5 +49,13 @@ describe('FacebookAuthenticationService', () => {
       expect(loadUserAccountRepository.load).toHaveBeenCalledTimes(1);
     });
 
+  test('should call CreateUserAccountRepository when LoadUserAccountReturns undefined', async() => {
+    loadUserAccountRepository.load.mockResolvedValueOnce(undefined)
+    await sut.perform({ token });
+    expect(createFacebookAccountRepository.createFromFacebook)
+    .toHaveBeenCalledWith({ name: 'any_facebook_name', email: 'any_facebook_email@mail.com',
+      facebookId: 'any_facebook_id'})
+    expect(createFacebookAccountRepository.createFromFacebook).toHaveBeenCalledTimes(1)
+    })
 });
 
